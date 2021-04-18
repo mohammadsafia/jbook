74cb42bc2336aebf7d2cf8830ce5fd81e876b39f
Mohammad Safia <msafia@terkwaz.com>
2021-04-18 21:44:32 +0300

    Split bundler code to folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,66 +1,23 @@
 
-import 'bulmaswatch/superhero/bulmaswatch.min.css'
-import { useState, useEffect, useRef } from 'react';
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
+
+import bundle from './bundler'
+
 import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
+
+
 const App = () => {
-  const ref = useRef<any>();
-  const iframe = useRef<any>();
+  const [code, setCode] = useState<string>('')
   const [input, setInput] = useState('');
 
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
-  useEffect(() => {
-    startService();
-  }, []);
-
   const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-    iframe.current.srcdoc = html;
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    });
-
-    // setCode(result.outputFiles[0].text);
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
-  };
-
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener('message', (event) => {
-            try{
-              eval(event.data);
-            }catch(err) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"> <h4>Runtime Error</h4>' + err +  '</div>';
-              console.error(err)
-            }
-          }, false);
-        </script>
-      </body>
-    </html>
-  `;
+    const output = await bundle(input)
+    setCode(output);
+  }
 
   return (
     <div>
@@ -69,7 +26,7 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+      <Preview code={code} />
     </div>
   );
 };

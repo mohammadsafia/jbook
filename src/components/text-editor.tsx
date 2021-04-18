f0e3a305a815bd342c3c8383cfb1e1a32b7dd402
Mohammad Safia <msafia@terkwaz.com>
2021-04-19 01:05:55 +0300

    Toggling the markdown editor mode

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
 export interface TextEditorProps {
@@ -5,9 +6,30 @@ export interface TextEditorProps {
 }
 
 const TextEditor: React.FC<TextEditorProps> = () => {
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = () => {
+      setEditing(false);
+    }
+
+    document.addEventListener('click', listener, { capture: true })
+    
+    return () => {
+      document.removeEventListener('click', listener, { capture: true })
+    }
+  }, [])
+
+  if (editing) {
+    return (
+      <MDEditor />
+    )
+  }
+
+
   return (
-    <div>
-      <MDEditor.Markdown source={'$'} />
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={'# Header'} />
     </div>
   );
 }

c155ff25bc69c65955e5b712f340e6383cc35f8f
Mohammad Safia <msafia@terkwaz.com>
2021-04-21 02:12:49 +0300

    Add some styles for cell-list

--- src/components/cell-list/cell-list.tsx
+++ src/components/cell-list/cell-list.tsx
@@ -1,4 +1,5 @@
-import { Fragment } from 'react'
+import './cell-list.css';
+import { Fragment } from 'react';
 import { useTypedSelector } from '../../hooks';
 import CellListItem from './cell-list-item';
 import AddCell from '../add-cell/add-cell'
@@ -15,7 +16,7 @@ const CellList: React.FC<CellListProps> = () => {
       <AddCell previousCellId={cell.id} />
     </Fragment>)
   return (
-    <div>
+    <div className="cell-list">
       <AddCell previousCellId={null} forceVisible={cells.length === 0} />
       {renderedCells}
     </div>
